cb720830d278d7056a36539685f8a7b6b0570b24
Nox <104575590+Wisterium@users.noreply.github.com>
2026-05-30 15:52:46 -0500

    Add files via upload

---
+++ asset/messages.js
@@ -0,0 +1,13 @@
+    const messages = [
+        "The moonlight is a messenger of love...",
+        "We could do so much better than this... Mausoleum faces and momentary bliss..",
+        "I've liked you for a thousand years, a thousand years..",
+        "Out of body from morning to night, dance floor packed and I'm feeling alright..",
+        "Road work ahead?? Yeah I sure hope it does????",
+        "it's me crewmate.. i am the good guy on the spaceship..",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    const randomMessage = messages[randomIndex];
+
+    document.getElementById("randomMessage").innerHTML = randomMessage;
