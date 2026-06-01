3977765be4ff1869e02e905bb7602405c91b8100
Nox <104575590+Wisterium@users.noreply.github.com>
2026-06-01 18:06:51 -0500

    Add files via upload

---
+++ js/navigation.js
@@ -0,0 +1,15 @@
+document.getElementById('links').innerHTML = `
+    <h1>LINKS</h1>
+    <hr>
+
+    <br>
+   <a href="https://wisterium.github.io/" id="navlink"> HOME </a>
+
+    <hr>
+    
+    <br>
+   <a href="aboutme.html" id="navlink"> ABOUT </a>
+
+   <hr>
+
+    `;
